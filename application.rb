012a20cefb32882e2430cbaa617e4e4db0820b40
Chris Jacob <i.chris.jacob@gmail.com>
2010-07-27 02:23:48 +1000

    added sinatra template as per http://github.com/jpatterson/heroku-sinatra-template

---
+++ application.rb
@@ -0,0 +1,23 @@
+require 'sinatra'
+
+configure :production do
+  # Configure stuff here you'll want to
+  # only be run at Heroku at boot
+
+  # TIP:  You can get you database information
+  #       from ENV['DATABASE_URI'] (see /env route below)
+end
+
+get '/' do
+  erb :index
+end
+
+# Test at <appname>.heroku.com
+
+# You can see all your app specific information this way.
+# IMPORTANT! This is a very bad thing to do for a production
+# application with sensitive information
+
+get '/env' do
+  ENV.inspect
+end
